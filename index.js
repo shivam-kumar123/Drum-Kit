c77b9f705f8d95abae9883200e0e30a3cdb32d23
shivam kumar <75497119+shivam-kumar123@users.noreply.github.com>
2022-04-22 23:43:30 +0530

    project completed
    computer keyboard functions added and effects are also added on clicking

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-for(let i=0;i<document.querySelectorAll(".drum").length;i++) document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+for(let i=0;i<document.querySelectorAll(".drum").length;i++) 
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
     if(i == 0) var audio = new Audio('sounds/tom-1.mp3');
     else if(i == 1) var audio = new Audio('sounds/tom-2.mp3');
     else if(i == 2) var audio = new Audio('sounds/tom-3.mp3');
@@ -8,4 +8,25 @@ for(let i=0;i<document.querySelectorAll(".drum").length;i++) document.querySelec
     else if(i == 5) var audio = new Audio('sounds/crash.mp3');
     else var audio = new Audio('sounds/kick-bass.mp3');
     audio.play();
+    buttonAnimation(this.innerHTML);
 });
+function makeSound(key){
+    if(key == 'w') var audio = new Audio('sounds/tom-1.mp3');
+    else if(key == 'a') var audio = new Audio('sounds/tom-2.mp3');
+    else if(key == 's') var audio = new Audio('sounds/tom-3.mp3');
+    else if(key == 'd') var audio = new Audio('sounds/tom-4.mp3');
+    else if(key == 'j') var audio = new Audio('sounds/snare.mp3');
+    else if(key == 'k') var audio = new Audio('sounds/crash.mp3');
+    else if(key == 'l') var audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+}
+document.addEventListener("keydown" , function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + currentKey).classList.remove("pressed");
+    },200);
+}
